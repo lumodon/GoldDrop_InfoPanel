6768a4fbc58324b642217c154d638373aaa879aa
Ubuntu <ubuntu@ip-172-30-0-171.us-west-2.compute.internal>
2019-03-25 17:59:13 +0000

    Fixes postgres production connection

--- src/db/pgp.js
+++ src/db/pgp.js
@@ -2,9 +2,9 @@ const path = require('path')
 require('dotenv').config({'path': path.join(__dirname, '../.env')})
 
 const cn = {
-  'host': '127.0.0.1',
+  'host': process.env.PG_HOST,
   'port': 5432,
-  'database': 'golddrop_distru',
+  'database': process.env.PG_DBNAME,
   'user': process.env.PG_USERNAME || 'username',
   'password': process.env.PG_PASSWORD || 'password',
 }
