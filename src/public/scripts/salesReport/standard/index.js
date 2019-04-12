7abe8ee0a9fab46e48f18a1ed4f855745b972742
Serafin Wesnidge <leaf466@gmail.com>
2019-04-12 15:00:26 -0700

    Updates style and adds detail

--- src/public/scripts/salesReport/standard/index.js
+++ src/public/scripts/salesReport/standard/index.js
@@ -1,6 +1,22 @@
+import * as moment from 'moment'
+window['moment'] = moment
+
+function dateIsLastMonth(dateAsEpoch) {
+  const beginningOfLastMonth = moment().startOf('month').subtract(1, 'month')
+  const endOfLastMonth = moment(beginningOfLastMonth).add(1, 'month').subtract(1, 'second')
+  const monthOfProvidedDate = moment(dateAsEpoch)
+  return monthOfProvidedDate.isAfter(beginningOfLastMonth) && monthOfProvidedDate.isBefore(endOfLastMonth)
+}
+
 function populate(data) {
+  const newData = []
+  for(const item of data.salesReport) {
+    if(dateIsLastMonth(Number(item.order_date_epoch) * 1000) && (item.sku.substr(0,2) === 'LT' || item.sku.substr(0,2) === 'BV')) {
+      newData.push(item)
+    }
+  }
   const skus = {}
-  for(const sale of data.salesReport) {
+  for(const sale of newData) {
     // HELP DEEPLY APPENDING WITH CHECKS!!
     if(skus[sale.sku]) {
       if(skus[sale.sku].sales && skus[sale.sku].sales[sale.cost_per_unit]) {
@@ -45,7 +61,7 @@ function populate(data) {
     const valuesList = skuContainer.querySelector('.values-list')
     for(const cost in skus[sku].sales) {
       const costContainer = document.createElement('div')
-      costContainer.classList.add('flex-container', 'vertical', 'cost-container', 'big-row-item', 'space-bottom')
+      costContainer.classList.add('flex-container', 'vertical', 'cost-container', 'big-row-item', 'space-bottom', 'fullborder')
       valuesList.appendChild(costContainer)
 
       costContainer.innerHTML = `
@@ -57,11 +73,13 @@ function populate(data) {
       try {
         for(const value of skus[sku].sales[cost].sales) {
           const itemItself = document.createElement('div')
-          itemItself.classList.add('item')
+          itemItself.classList.add('item', 'flex-container', 'vertical')
           costList.appendChild(itemItself)
 
           itemItself.innerHTML = `
             <span>Quantity: ${value.quantity}</span>
+            <span>Customer: ${value.customer}</span>
+            <span style="margin-bottom: 10px;">Date: ${moment(Number(value.order_date_epoch) * 1000).calendar()}</span>
           `
         }
       } catch(e) {
